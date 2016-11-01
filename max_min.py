e436ec9cafe898a9d5b4a0db39f3f5e2b9451a18
Stephen Oyaro <vystoya@gmail.com>
2016-11-01 16:32:12 +0300

    Put comments in one line for cleaner code

--- max_min.py
+++ max_min.py
@@ -1,5 +1,4 @@
-def find_max_min(numbers): """passed numbers as an argument that will
-							  later be the list that the 'if' will be using"""
+def find_max_min(numbers): # passed numbers as an argument that will later be the list that the 'if' will be using
 	numbers is list
 	if(len(numbers) > 1): # There has to be atleast two items in the list for comparison of the min() max()
 	    if(max(numbers) == min(numbers)):
